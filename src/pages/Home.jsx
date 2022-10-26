68e7bed9bee797773f955b9ea041aefae707d568
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-10-26 11:21:00 +0200

    Correction page Home OK

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../assets/accueil-fond.png";
 import Layout from "./Layout";
 import Banner from "../components/Accueil_Banner";
 import Card from "../components/Card";
